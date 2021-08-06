5a730a25b1379617f63940b638f5389c6750c3eb
contact2satish.patna@gmail.com <contact2satish.patna@gmail.com>
2021-08-06 21:24:38 +0530

    setup nodejs

---
+++ global/globalMiddlewares.js
@@ -0,0 +1,9 @@
+/**
+ * `globalMiddlewares` handles all the middlewares/functions/configurations that you need
+ * to declare/use in your application globally.
+ * @param {import ("kvell-scripts").KvellAppObject} app
+ * @param {import ("kvell-scripts").KvellServerObject} server
+ */
+const globalMiddlewares = (app, server) => {};
+
+module.exports = globalMiddlewares;
---
+++ kvell.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  protocol: "http",
+  routes: [],
+  models: [],
+  autoRequireRoutes: true,
+  registerDocsRoute: true
+};
